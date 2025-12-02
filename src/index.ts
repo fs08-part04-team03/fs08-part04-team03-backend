6cccf779b5d1b4b660a53ebbb880c6f0f80f8902
cuckooolly <cuckooolly@gmail.com>
2025-12-02 10:50:49 +0900

    fix: use TypeScript type imports for express in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { type Application, type Request, type Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
