6d0434a8c5676caba7de5589fbc5ed83c5c7baff
Gibum <gudeng0611@cau.ac.kr>
2025-12-17 11:16:12 +0900

    refactor: remove legacy comapny user retrieval and name change
     - 회사 소속 유저 조회 및 회사명 조회와 관련된 레거시 코드를 삭제
       - company api 중 user로 이관된 /users, /name 삭제

--- src/domains/company/company.controller.ts
+++ src/domains/company/company.controller.ts
@@ -1,13 +1,10 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { CustomError } from '../../common/utils/error.util';
 import { HttpStatus } from '../../common/constants/httpStatus.constants';
 import { ErrorCodes } from '../../common/constants/errorCodes.constants';
 import { ResponseUtil } from '../../common/utils/response.util';
 import type { AuthenticatedRequest } from '../../common/types/common.types';
 import { companyService } from './company.service';
-import type { CompanyUserQuery, UpdateCompanyNameBody } from './company.types';
-
-type GetUsersRequest = AuthenticatedRequest & Request<unknown, unknown, unknown, CompanyUserQuery>;
 
 // 회사 ID 가져오기
 const getCompanyId = (req: AuthenticatedRequest) => {
@@ -27,29 +24,5 @@ export const companyController = {
   getDetail: async (req: AuthenticatedRequest, res: Response) => {
     const company = await companyService.getCompanyDetail(getCompanyId(req));
     res.status(HttpStatus.OK).json(ResponseUtil.success(company, '회사 정보 조회 성공'));
-  },
-
-  // 회사 소속 유저 조회 (Admin 전용)
-  getUsers: async (req: GetUsersRequest, res: Response) => {
-    const { role, isActive, page, limit } = req.query;
-    const result = await companyService.getCompanyUsers(getCompanyId(req), {
-      role,
-      isActive,
-      page,
-      limit,
-    });
-    res
-      .status(HttpStatus.OK)
-      .json(
-        ResponseUtil.successWithPagination(result.users, result.pagination, '소속 유저 조회 성공')
-      );
-  },
-
-  // 회사명 변경 (Admin 전용)
-  updateName: async (req: AuthenticatedRequest, res: Response) => {
-    const companyId = getCompanyId(req);
-    const { name } = req.body as UpdateCompanyNameBody;
-    const updated = await companyService.updateCompanyName(companyId, { name });
-    res.status(HttpStatus.OK).json(ResponseUtil.success(updated, '회사명 변경 성공'));
   },
 };
